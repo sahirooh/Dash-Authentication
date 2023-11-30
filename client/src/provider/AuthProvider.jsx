d84da127565a9fdb6c63e24cba6e59b2baabcb52
sahirooh <sahirooh@gmail.com>
2023-11-30 23:58:56 +0530

    updated authprovider

--- client/src/provider/AuthProvider.jsx
+++ client/src/provider/AuthProvider.jsx
@@ -4,25 +4,31 @@ import jwt_decode from "jwt-decode";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const [token, setToken] = useState(null);
-    
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
-    const storeToken = (token) => {
-        const decodedToken = jwt_decode(token);
-        setToken(decodedToken);
+  const login = (user, token) => {
+    if (token) {
+      storeToken(token);
     }
+    const decodedToken = jwt_decode(token);
+    console.log(decodedToken);
+  };
 
-    const login = (user, token) => {
-
-    }
-
-    const logout = () => {
-        setUser(null);
-        setToken(null);
-    }
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+  };
+  
   return (
-    <AuthContext.Provider value={{ name: "Jorge" }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        token,
+        login,
+        logout,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
