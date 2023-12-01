398ec5e441ec3ef1afc61d73b48924b10ba21e30
sahirooh <sahirooh@gmail.com>
2023-12-01 23:57:37 +0530

    updated authProvider

--- client/src/provider/AuthProvider.jsx
+++ client/src/provider/AuthProvider.jsx
@@ -1,23 +1,34 @@
 import { createContext, useState } from "react";
 import {jwtDecode} from "jwt-decode";
+import { useCookies } from "react-cookie";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [cookies, setCookie, removeCookie] = useCookies();
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
 
-  const login = ( token) => {
-    if (token) {
+  const login = ( tokenStr) => {
+    if (tokenStr) {
       setToken(token);
     }
-    const decodedToken = jwtDecode(token);
-    console.log(decodedToken);
+    const {exp} = jwtDecode(tokenStr);
+    if(exp){
+      setCookie("jwt", tokenStr, {
+        path: "/",
+        maxAge: exp,
+        sameSite: true,
+      });
+      return;
+    }
+    logout();
   };
 
   const logout = () => {
     setUser(null);
     setToken(null);
+    removeCookie("jwt", {path: "/"});
   };
   
   return (
